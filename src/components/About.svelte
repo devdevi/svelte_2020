f36d5a695339c09f8d56707501d9a9548c07c22f
Visaka <visaka.chaves@consorcio.cl>
2020-04-15 23:20:44 -0400

    Condicionales

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -5,17 +5,31 @@
     function handleClick() {
         count += 1
     }
+    let styles = { darkMode: false }
+
+    function toggle() {
+        styles.darkMode = !styles.darkMode;
+        console.log(styles.darkMode)
+        window.document.body.classList.toggle("dark-mode");
+    }
 
 </script>
 
 <style>
-    .about{
+    .About{
         font-size: 3rem;
         color:  var(--theme-color);
     }
 </style>
 
-<div class="about">
+<div class="About">
+
+{#if !styles.darkMode}
+
 <p>{someText}</p>
+{:else}
+<p>Antes yo te queria pero ya no</p>
+ {/if}
 <button on:click={handleClick} >Sumar {count === 0 ? '' : count}</button>
+<button on:click={toggle}>Dark-mode</button>
 </div>
